bf3d6e85a10f6366e7357b420d4b1a5ae46efa9e
Tim Dettrick <t.dettrick@uq.edu.au>
2014-04-10 18:11:19 +1000

    Fixing up whitespace.

--- app/Global.scala
+++ app/Global.scala
@@ -2,11 +2,11 @@ import com.google.inject._
 import play.api.GlobalSettings
 
 object Global extends GlobalSettings {
-    
+
   val injector = Guice.createInjector(new AbstractModule {
     def configure {}
   })
-  
+
   override def getControllerInstance[A](controllerClass: Class[A]): A =
-      injector.getInstance(controllerClass)
+    injector.getInstance(controllerClass)
 }
