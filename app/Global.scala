3cf58a829ffc3638e4a3a71c77edd65905b9102d
Tim Dettrick <t.dettrick@uq.edu.au>
2014-04-10 17:43:50 +1000

    Initial commit on new Play 2.2.2 project with Guice.

---
+++ app/Global.scala
@@ -0,0 +1,12 @@
+import com.google.inject._
+import play.api.GlobalSettings
+
+object Global extends GlobalSettings {
+    
+  val injector = Guice.createInjector(new AbstractModule {
+    def configure {}
+  })
+  
+  override def getControllerInstance[A](controllerClass: Class[A]): A =
+      injector.getInstance(controllerClass)
+}
---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "dit4c-highcommand"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  cache,
+  "com.google.inject" % "guice" % "3.0"
+)     
+
+play.Project.playScalaSettings
