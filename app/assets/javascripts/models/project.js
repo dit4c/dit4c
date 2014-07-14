bb66ff12655339ce1066f83d2e7113ac9ccbff3c
Tim Dettrick <t.dettrick@uq.edu.au>
2014-07-14 10:26:39 +1000

    Removing old files.

--- app/assets/javascripts/models/project.js
+++
@@ -1,8 +0,0 @@
-App.Project = DS.Model.extend({
-  name: DS.attr('string'),
-  image: DS.attr('string'),
-  active: DS.attr('boolean'),
-  rootUrl: function() {
-    return "//"+this.get('name')+"."+window.location.host;
-  }.property('name')
-});
