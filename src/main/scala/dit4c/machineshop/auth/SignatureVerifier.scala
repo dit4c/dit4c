de6ebeefcb2e3673e88b9153ffb850148f3bca3f
Tim Dettrick <t.dettrick@uq.edu.au>
2014-07-23 11:58:47 +1000

    Implemented basic checker for HTTP signatures.
    Based on the spec at:
    https://web-payments.org/specs/source/http-signatures/

--- src/main/scala/dit4c/machineshop/auth/SignatureVerifier.scala
+++ src/main/scala/dit4c/machineshop/auth/SignatureVerifier.scala
@@ -2,9 +2,96 @@ package dit4c.machineshop.auth
 
 import com.nimbusds.jose.jwk.JWKSet
 import scala.collection.JavaConversions._
+import spray.http.HttpRequest
+import java.security.Signature
+import spray.http.HttpHeaders
+import spray.http.GenericHttpCredentials
+import scala.collection.JavaConversions._
+import com.nimbusds.jose.util.Base64
 
+// https://web-payments.org/specs/source/http-signatures/
 class SignatureVerifier(publicKeys: JWKSet) {
 
-  publicKeys.getKeys.toSeq
+  def apply(request: HttpRequest): Either[String, Unit] = {
+    extractSignatureInfo(request)
+      .right.flatMap { info =>
+        // Get public key from keyset
+        Option(publicKeys.getKeyByKeyId(info.keyId)).map { key =>
+          // Load public key, input signing string and verify
+          info.algorithm.initVerify(
+              key.asInstanceOf[com.nimbusds.jose.jwk.RSAKey].toRSAPublicKey)
+          info.algorithm.update(info.signingString(request).getBytes)
+          // Perform the validation
+          if (info.algorithm.verify(info.signature)) {
+            Right()
+          } else {
+            Left("HTTP Signature was invalid.")
+          }
+        }.getOrElse(Left("Public key with required ID was not found."))
+      }
+  }
+
+  private def extractSignatureInfo(
+      request: HttpRequest): Either[String, SignatureAuthorizationHeader] = {
+    request.header[HttpHeaders.Authorization]
+      .map(Right(_))
+      .getOrElse(Left("No \"Authorization\" header present."))
+      .right.flatMap { header =>
+        header.credentials match {
+          case c: GenericHttpCredentials if (c.scheme == "Signature") =>
+            Right(c.params)
+          case _ =>
+            Left("Authorization is not a HTTP Signature.")
+        }
+      }
+      .right.flatMap(SignatureAuthorizationHeader.fromParams)
+  }
+
+  private def getSignatureInstance(algorithm: String): Option[Signature] =
+    (algorithm match {
+      case "rsa-sha1"   => Some("SHA1withRSA")
+      case "rsa-sha256" => Some("SHA256withRSA")
+      case _ => None
+    }).map(Signature.getInstance(_))
+
+  private case class SignatureAuthorizationHeader(
+        keyId: String,
+        algorithm: java.security.Signature,
+        headers: Seq[String],
+        signature: Array[Byte]
+      ) {
+    def signingString(req: HttpRequest): String = {
+      def values(name: String): Seq[String] =
+        if (name == "(request-target)")
+          Seq(s"${req.method.value.toLowerCase} ${req.uri.path}")
+        else
+          req.headers
+            .filter(h => h.lowercaseName == name) // Headers with name
+            .map(_.value.trim) // Convert to strings and trim (as per spec)
+      // Fetch headers in order, repeating headers when necessary
+      (for (hn <- headers; hv <- values(hn)) yield s"$hn: $hv").mkString("\n")
+    }
+  }
+
+  private object SignatureAuthorizationHeader {
+    def fromParams(
+          m: Map[String, String]
+        ): Either[String, SignatureAuthorizationHeader] = {
+      val requiredParams = Set("keyId", "algorithm", "signature")
+      if (requiredParams.forall(m.contains)) {
+        val keyId = m("keyId")
+        val possibleAlgorithm = getSignatureInstance(m("algorithm"))
+        // "headers" is optional, with default of Date header only.
+        // https://web-payments.org/specs/source/http-signatures/#headers
+        val headers = m.get("headers").getOrElse("date").split(" ")
+        val signature = (new Base64(m("signature"))).decode
+        possibleAlgorithm.map { algorithm =>
+          SignatureAuthorizationHeader(keyId, algorithm, headers, signature)
+        }.toRight("Unsupported signature algorithm.")
+      } else {
+        Left(s"HTTP Signature header does not contain all required fields.")
+      }
+    }
+  }
 
 }
--- src/test/scala/dit4c/machineshop/auth/SignatureVerifierSpec.scala
+++ src/test/scala/dit4c/machineshop/auth/SignatureVerifierSpec.scala
@@ -1,18 +1,26 @@
 package dit4c.machineshop.auth
 
+import java.math.BigInteger
 import java.security.KeyPairGenerator
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
+import java.security.KeyFactory
+import java.security.spec.RSAPrivateCrtKeySpec
+import java.security.Signature
 import java.util.concurrent.TimeUnit
-
 import org.specs2.mutable.Specification
-
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.Use
-
+import spray.httpx.RequestBuilding._
 import akka.util.Timeout
+import spray.http.HttpRequest
+import spray.http.HttpHeaders
+import spray.http.GenericHttpCredentials
+import spray.http.DateTime
+import com.nimbusds.jose.util.Base64
+import com.nimbusds.jose.util.Base64URL
 
 class SignatureVerifierSpec extends Specification {
   import scala.concurrent.ExecutionContext.Implicits.global
@@ -21,6 +29,7 @@ class SignatureVerifierSpec extends Specification {
   "SignatureVerifier" >> {
 
     "verifies a JWT " >> {
+      val keyId = "Test RSA key (512 bit)"
       val keySet = {
         val generator = KeyPairGenerator.getInstance("RSA")
         generator.initialize(512) // Yes, it's weak, but it's fast!
@@ -28,13 +37,66 @@ class SignatureVerifierSpec extends Specification {
         val pub = kp.getPublic.asInstanceOf[RSAPublicKey]
         val priv = kp.getPrivate.asInstanceOf[RSAPrivateKey]
         val k = new RSAKey(pub, priv, Use.SIGNATURE, JWSAlgorithm.parse("RSA"),
-            "Test RSA key (512 bit)", null, null, null)
+            keyId, null, null, null)
         new JWKSet(k)
       }
 
-      new SignatureVerifier(keySet.toPublicJWKSet)
+      def addSignature(requiredHeaders: Seq[String]) =
+        { req: HttpRequest =>
+          def headerValue(name: String) =
+            if (name == "(request-target)")
+              s"${req.method.value.toLowerCase} ${req.uri.path}"
+            else
+              req.headers
+                .find(h => h.name.toLowerCase == name) // First header with name
+                .map(_.value).getOrElse("") // Convert to string
 
-      pending
+          val signingString = requiredHeaders
+            .map { hn => s"$hn: ${headerValue(hn)}" }
+            .mkString("\n")
+
+          val algorithm = "rsa-sha256"
+
+          def signature(signingString: String): Base64 = {
+            def getSignatureInstance(alg: String): Signature =
+              Signature.getInstance(alg match {
+                case "rsa-sha1"   => "SHA1withRSA"
+                case "rsa-sha256" => "SHA256withRSA"
+              })
+
+            val privateKey =
+              keySet.getKeyByKeyId(keyId).asInstanceOf[RSAKey].toRSAPrivateKey
+
+            val sig = getSignatureInstance(algorithm)
+            sig.initSign(privateKey)
+            sig.update(signingString.getBytes)
+
+            Base64.encode(sig.sign)
+          }
+
+          req ~> addHeader(HttpHeaders.Authorization(
+            GenericHttpCredentials("Signature", "", Map(
+              "keyId" -> keyId,
+              "algorithm" -> algorithm,
+              "headers" -> requiredHeaders.mkString(" "),
+              "signature" -> signature(signingString).toString
+            ))
+          ))
+        }
+
+      val request = Post("/create?name=foo", "") ~>
+        addHeader(HttpHeaders.Date(DateTime.now)) ~>
+        addHeader(HttpHeaders.`Content-Length`(0)) ~>
+        addSignature(Seq("(request-target)", "date", "content-length"))
+
+      val verifier = new SignatureVerifier(keySet.toPublicJWKSet)
+
+      verifier(request) must beRight
+
+      verifier(
+        request
+          ~> removeHeader("Date")
+          ~> addHeader(HttpHeaders.Date(DateTime(0)))) must beLeft[String]
     }
   }
 }
