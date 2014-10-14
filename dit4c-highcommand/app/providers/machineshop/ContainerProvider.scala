163e91d86f0343990c157e3a6ef7dddd161c5c6d
Tim Dettrick <t.dettrick@uq.edu.au>
2014-10-14 17:42:12 +1000

    Refactored lots of the ContainerProvider logic into a simplified client.

--- dit4c-highcommand/app/providers/machineshop/ContainerProvider.scala
+++ dit4c-highcommand/app/providers/machineshop/ContainerProvider.scala
@@ -2,85 +2,70 @@ package providers.machineshop
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
-import play.api.libs.ws.WSRequestHolder
 import play.api.libs.json._
+import play.api.libs.ws._
 import scala.util.Try
-import java.util.Date
-import java.util.TimeZone
-import play.api.libs.ws.WSRequestHolder
 import com.nimbusds.jose.jwk.RSAKey
-import java.security.Signature
-import com.nimbusds.jose.util.Base64
-import play.api.libs.ws.InMemoryBody
-import java.security.MessageDigest
-import play.api.libs.ws.WS
 
 class ContainerProvider(
     managementUrl: String,
-    privateKey: () => Future[RSAKey]
+    privateKeyProvider: () => Future[RSAKey]
   )(implicit executionContext: ExecutionContext) {
 
   import play.api.libs.functional.syntax._
   import play.api.Play.current
-  import ContainerProvider._
+  import MachineShop.Client
   import MachineShop.Container
 
-  def create(name: String, image: String) = withPrivateKey { key =>
-    ws("containers/new")
-      .withMethod("POST")
-      .withHeaders("Content-Type" -> "application/json; charset=utf-8")
-      .withBody(InMemoryBody(Json.stringify(
-          Json.obj("name" -> name, "image" -> image)).getBytes))
-      .calculateDigest
-      .httpSign(key)
-      .execute()
-      .map(_.json.as[Container])
-  }
+  val client = new Client(managementUrl, privateKeyProvider)
 
-  def get(name: String): Future[Option[Container]] =
-    ws(s"containers/$name")
-      .get()
+  def create(name: String, image: String) =
+    client("containers/new")
+      .signed { ws: WSRequestHolder =>
+        ws.withMethod("POST")
+          .withHeaders("Content-Type" -> "application/json; charset=utf-8")
+          .withBody(InMemoryBody(Json.stringify(
+              Json.obj("name" -> name, "image" -> image)).getBytes))
+      }
+      .map(_.json.as[Container])
+
+  def get(name: String): Future[Option[Container]] = 
+    client(s"containers/$name")
+      .unsigned(_.withMethod("GET"))
       .map(r => Try(r.json.as[Container]).toOption)
 
   def list: Future[Seq[Container]] =
-    ws("containers").get().map { response =>
-      response.json.asInstanceOf[JsArray].value.map(_.as[Container])
-    }
+    client("containers")
+      .unsigned(_.withMethod("GET"))
+      .map { response =>
+        response.json.asInstanceOf[JsArray].value.map(_.as[Container])
+      }
 
   class ContainerImpl(
       val name: String,
       val active: Boolean
       )(implicit executionContext: ExecutionContext) extends Container {
 
-    override def start: Future[Container] = withPrivateKey { key =>
-      ws(s"containers/$name/start")
-        .withMethod("POST")
-        .httpSign(key)
-        .execute()
+    override def start: Future[Container] =
+      client(s"containers/$name/start")
+        .signed(_.withMethod("POST"))
         .map(_.json.as[Container])
-    }
 
-    override def stop: Future[Container] = withPrivateKey { key =>
-      ws(s"containers/$name/stop")
-        .withMethod("POST")
-        .httpSign(key)
-        .execute()
+    override def stop: Future[Container] =
+      client(s"containers/$name/stop")
+        .signed(_.withMethod("POST"))
         .map(_.json.as[Container])
-    }
 
-    override def delete: Future[Unit] = withPrivateKey { key =>
+    override def delete: Future[Unit] =
       stop.flatMap { _ =>
-        ws(s"containers/$name")
-          .withMethod("DELETE")
-          .httpSign(key)
-          .execute()
+        client(s"containers/$name")
+          .signed(_.withMethod("DELETE"))
           .flatMap { response =>
             if (response.status == 204) Future.successful[Unit](Unit)
             else Future.failed(
                 new Exception(s"Deletion failed: ${response.status}"))
           }
       }
-    }
 
     override def toString = s"ContainerProvider.Container($name, $active)"
 
@@ -91,68 +76,4 @@ class ContainerProvider(
     (__ \ "active").read[Boolean]
   )((new ContainerImpl(_,_)))
 
-  protected def withPrivateKey[A](f: RSAKey => Future[A]): Future[A] =
-    privateKey().flatMap(f)
-
-  protected def ws(path: String) = WS.url(s"$managementUrl$path")
-
-}
-
-object ContainerProvider {
-  implicit class HttpSignatureCalculator(request: WSRequestHolder) {
-    def httpSign(privateKey: RSAKey): WSRequestHolder = {
-      def now = {
-        val sdf = new java.text.SimpleDateFormat(
-            "E, dd MMM yyyy HH:mm:ss z", java.util.Locale.US)
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"))
-        sdf.format(new Date())
-      }
-
-      // Horribly procedural - rewrite it later
-      var req = request
-
-      if (req.headers.get("Date").isEmpty) {
-        req = req.withHeaders("Date" -> now)
-      }
-
-      val uriPattern = """^\w+\:\/\/[^/]+(.+)$""".r
-      val uriPattern(uri) = req.url
-      val headerNames =
-        if (req.headers.contains("Digest")) List("Date", "Digest")
-        else List("Date")
-
-      val signingLines =
-        s"(request-target): ${req.method.toLowerCase} $uri" ::
-        headerNames.map { hn =>
-          req.headers(hn).map(hv => s"${hn.toLowerCase}: $hv").toList
-        }.reduce(_ ++ _)
-      val signingString = signingLines.mkString("\n")
-
-      val sig = Signature.getInstance("SHA256withRSA")
-      sig.initSign(privateKey.toRSAPrivateKey)
-      sig.update(signingString.getBytes)
-
-      val params: Map[String, String] = Map(
-        "keyId" -> privateKey.getKeyID,
-        "algorithm" -> "rsa-sha256",
-        "headers" -> ("(request-target)" :: headerNames).map(_.toLowerCase).mkString(" "),
-        "signature" -> Base64.encode(sig.sign).toString
-      )
-
-      req.withHeaders(
-        "Authorization" -> ("Signature " +
-          params.map({case (k,v) => s"""$k="$v""""}).mkString(","))
-      )
-    }
-
-    def calculateDigest: WSRequestHolder = request.body match {
-      case InMemoryBody(bytes) =>
-        val digest = MessageDigest.getInstance("SHA-256")
-        digest.update(bytes)
-        request.withHeaders(
-          "Digest" -> s"SHA-256=${Base64.encode(digest.digest)}"
-        )
-      case _ => request // Can't calculate easily
-    }
-  }
 }
--- dit4c-highcommand/app/providers/machineshop/MachineShop.scala
+++ dit4c-highcommand/app/providers/machineshop/MachineShop.scala
@@ -3,7 +3,14 @@ package providers.machineshop
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 import scala.util.Try
-import play.api.libs.ws.WS
+
+import java.security.MessageDigest
+import java.security.Signature
+import java.util.Date
+import java.util.TimeZone
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.util.Base64
+import play.api.libs.ws._
 
 object MachineShop {
   trait Container {
@@ -25,5 +32,89 @@ object MachineShop {
       response <- call.get
     } yield response.body.trim
   }
+  
+  class Client(
+      managementUrl: String,
+      privateKeyProvider: () => Future[RSAKey]
+      )(implicit executionContext: ExecutionContext) {
+    
+    import play.api.Play.current
+    
+    def apply(path: String) = new RequestHolder(WS.url(s"$managementUrl$path"))  
+    
+    class RequestHolder(ws: WSRequestHolder) {
+      type PrepFunc = WSRequestHolder => WSRequestHolder
+      
+      def signed(prepareFunc: PrepFunc): Future[WSResponse] =
+        for {
+          key <- privateKeyProvider()
+          signedRequest = prepareFunc(ws).calculateDigest.httpSign(key)
+          result <- signedRequest.execute()
+        } yield result
+
+      def unsigned(prepareFunc: PrepFunc): Future[WSResponse] =
+        prepareFunc(ws).calculateDigest.execute()
+
+    }
+      
+  }
+  
+  implicit class HttpSignatureCalculator(request: WSRequestHolder) {
+    def httpSign(privateKey: RSAKey): WSRequestHolder = {
+      def now = {
+        val sdf = new java.text.SimpleDateFormat(
+            "E, dd MMM yyyy HH:mm:ss z", java.util.Locale.US)
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"))
+        sdf.format(new Date())
+      }
+
+      // Horribly procedural - rewrite it later
+      var req = request
+
+      if (req.headers.get("Date").isEmpty) {
+        req = req.withHeaders("Date" -> now)
+      }
+
+      val uriPattern = """^\w+\:\/\/[^/]+(.+)$""".r
+      val uriPattern(uri) = req.url
+      val headerNames =
+        if (req.headers.contains("Digest")) List("Date", "Digest")
+        else List("Date")
+
+      val signingLines =
+        s"(request-target): ${req.method.toLowerCase} $uri" ::
+        headerNames.map { hn =>
+          req.headers(hn).map(hv => s"${hn.toLowerCase}: $hv").toList
+        }.reduce(_ ++ _)
+      val signingString = signingLines.mkString("\n")
+
+      val sig = Signature.getInstance("SHA256withRSA")
+      sig.initSign(privateKey.toRSAPrivateKey)
+      sig.update(signingString.getBytes)
+
+      val params: Map[String, String] = Map(
+        "keyId" -> privateKey.getKeyID,
+        "algorithm" -> "rsa-sha256",
+        "headers" -> ("(request-target)" :: headerNames).map(_.toLowerCase).mkString(" "),
+        "signature" -> Base64.encode(sig.sign).toString
+      )
+
+      req.withHeaders(
+        "Authorization" -> ("Signature " +
+          params.map({case (k,v) => s"""$k="$v""""}).mkString(","))
+      )
+    }
+
+    def calculateDigest: WSRequestHolder = request.body match {
+      case InMemoryBody(bytes) =>
+        val digest = MessageDigest.getInstance("SHA-256")
+        digest.update(bytes)
+        request.withHeaders(
+          "Digest" -> s"SHA-256=${Base64.encode(digest.digest)}"
+        )
+      case _ => request // Can't calculate easily
+    }
+  }
+  
 
 }
