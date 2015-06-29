516d8b4cf0fa4bc143fdf760302723e506844837
Tim Dettrick <t.dettrick@uq.edu.au>
2015-06-29 15:43:33 +1000

    Adding option to configure Docker host

--- dit4c-gatehouse/src/main/scala/dit4c/gatehouse/Boot.scala
+++ dit4c-gatehouse/src/main/scala/dit4c/gatehouse/Boot.scala
@@ -9,6 +9,7 @@ import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
 import java.io.File
+import java.net.URI
 import java.util.concurrent.TimeUnit
 import spray.http.Uri
 import dit4c.gatehouse.docker.DockerClient
@@ -24,7 +25,7 @@ object Boot extends App with SimpleRoutingApp {
 
   def start(config: Config) {
     startServer(interface = config.interface, port = config.port) {
-      val dockerClient = new DockerClient(Uri("http://127.0.0.1:2375/"))
+      val dockerClient = new DockerClient(Uri(config.dockerHost.toString))
       val dockerIndex = actorRefFactory.actorOf(
           Props(classOf[DockerIndexActor], dockerClient), "docker-index")
       val auth = actorRefFactory.actorOf(
@@ -52,7 +53,8 @@ object Boot extends App with SimpleRoutingApp {
 case class Config(
   val interface: String = "localhost",
   val port: Int = 8080,
-  val keyLocation: java.net.URI = null,
+  val dockerHost: URI = URI.create("http://127.0.0.1:2375/"),
+  val keyLocation: URI = null,
   val keyUpdateInterval: FiniteDuration = Duration.create(1, TimeUnit.HOURS))
 
 object ArgParser extends scopt.OptionParser[Config]("dit4c-gatehouse") {
@@ -63,7 +65,10 @@ object ArgParser extends scopt.OptionParser[Config]("dit4c-gatehouse") {
   opt[Int]('p', "port")
     .action { (x, c) => c.copy(port = x) }
     .text("port to listen on")
-  opt[java.net.URI]('s', "signed-by")
+  opt[URI]('H', "docker-host")
+    .action { (x, c) => c.copy(keyLocation = x) }
+    .text("URL/file of JWK RSA keyset used to sign JWT tokens")
+  opt[URI]('s', "signed-by")
     .required()
     .action { (x, c) => c.copy(keyLocation = x) }
     .text("URL/file of JWK RSA keyset used to sign JWT tokens")
