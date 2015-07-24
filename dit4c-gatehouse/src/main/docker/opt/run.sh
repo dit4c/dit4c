ab8174601ed933333d48148056e6968808447e4f
Tim Dettrick <t.dettrick@uq.edu.au>
2015-07-24 15:39:25 +1000

    Adding configurable Docker, gatehouse and machineshop variables

--- dit4c-gatehouse/src/main/docker/opt/run.sh
+++ dit4c-gatehouse/src/main/docker/opt/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "set \$portal \"$PORTAL_URL\";" > /etc/nginx/conf.d/portal_url.conf
+export DOCKER_ADDR=${DOCKER_ADDR:-172.17.42.1}
+export GATEHOUSE_URL=${GATEHOUSE_URL:-"http://127.0.0.1:8080"}
+export MACHINESHOP_URL=${MACHINESHOP_URL:-"http://172.17.42.1:8080"}
+
+cat > /etc/nginx/conf.d/variables.conf <<VARIABLES
+set \$portal "$PORTAL_URL";
+set \$docker "$DOCKER_ADDR";
+set \$gatehouse "$GATEHOUSE_URL";
+set \$machineshop "$MACHINESHOP_URL";
+VARIABLES
 
 nginx -t
 
