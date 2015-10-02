a46ab6af5fe2c4eca85de0650f47f46a2ee86946
Tim Dettrick <t.dettrick@uq.edu.au>
2015-10-02 16:57:20 +1000

    Improving JS caching and enabling h2 server push
    The Application controller observes requests for JavaScript assets that
    are not versioned, and sets up RequireJS config statements to make sure
    they're used. This works around the problem of not being able to easily
    enumerate JS assets at compile time.
    
    This cache of versioned JavaScript is also used to add a "Link" tag
    marking them as assets for preload. When used in conjunction with
    nghttpx this is used to perform server push of the assets to improve
    page load time.

--- dit4c-highcommand/app/controllers/Application.scala
+++ dit4c-highcommand/app/controllers/Application.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import com.google.inject.Inject
+import javax.inject.{Inject,Singleton}
 import providers.auth.AuthProviders
 import providers.db.CouchDB
 import scala.concurrent.duration.Duration
@@ -11,22 +11,54 @@ import scala.concurrent.Future.successful
 import play.api.libs.concurrent.Akka
 import play.api.libs.Codecs
 import scala.util.Try
+import play.api.http.ContentTypes
+import play.api.libs.json._
 
+@Singleton
 class Application @Inject() (
     authProviders: AuthProviders,
     webConfig: providers.WebConfig,
     val db: CouchDB.Database)
     extends Controller with Utils {
 
-  import http.HeaderNames.CACHE_CONTROL
-  
+  import http.HeaderNames._
+
   private val mainTmplETag = classBasedETag(views.html.main.getClass)
   private val waitingTmplETag = classBasedETag(views.html.main.getClass)
 
+  private val seenJsAssets = akka.agent.Agent(Set.empty[String])
+
   def main(path: String) = Action { implicit request =>
-    ifNoneMatch(mainTmplETag("")) {
-      Ok(views.html.main(authProviders.providers.toSeq, webConfig))
+    val jsAssets = seenJsAssets.get.toSeq.sorted
+    ifNoneMatch(mainTmplETag(jsAssets.mkString(","))) {
+      val linkHeader: Option[String] =
+        Option(
+          jsAssets
+            .map(path => routes.Assets.versioned(s"javascripts/$path").toString)
+            .map(v => s"<$v>; rel=preload")
+            .mkString(", ")
+          ).filter(_ != "")
+      Ok(views.html.main(authProviders.providers.toSeq, webConfig, requireJsConfig(jsAssets)))
         .withHeaders(CACHE_CONTROL -> "public, max-age=1")
+        .withHeaders(linkHeader.map("Link" -> _).toSeq: _*)
+    }
+  }
+
+  def jsAsset(file: Assets.Asset) = Action.async { implicit request =>
+    val path = request.path.stripPrefix(routes.Application.jsAsset("").toString)
+    val isVersioned = request.path.split("/").last.takeWhile(_ != '-') match {
+      case s if s.length >= 20 && s.matches("[0-9a-f]+") => true
+      case _ => false
+    }
+    Assets.versioned("/public/javascripts", file)(request).map { response =>
+
+      if (!isVersioned &&
+          response.header.status == 200 &&
+          response.header.headers(CONTENT_TYPE).contains("javascript")) {
+        seenJsAssets.send(_ + path)
+      }
+
+      response
     }
   }
 
@@ -47,7 +79,7 @@ class Application @Inject() (
         case _ =>
           val scheduler = Akka.system(Play.current).scheduler
           val waitTime = Duration(5, "seconds")
-          after(waitTime, scheduler) { 
+          after(waitTime, scheduler) {
             successful {
               Redirect(url.toString, 302)
                 .withHeaders(CACHE_CONTROL -> "no-cache")
@@ -78,7 +110,7 @@ class Application @Inject() (
 
   def classBasedETag(c: Class[_]) = {
     val urlConn = c.getResource(c.getSimpleName + ".class").openConnection
-    val base = 
+    val base =
       try {
         Codecs.sha1 {
           urlConn.getURL.toExternalForm + urlConn.getLastModified
@@ -88,5 +120,17 @@ class Application @Inject() (
       }
     { suffix: String => Codecs.sha1(base + suffix) }
   }
-    
+
+  def requireJsConfig(jsAssets: Seq[String]): String = {
+    val extra =
+      Json.obj(
+        "paths" -> jsAssets.foldLeft(Json.obj()) { case (obj, path) =>
+          obj ++ Json.obj(path.stripSuffix(".js") -> routes.Assets.versioned(s"javascripts/$path").toString.stripPrefix(routes.Application.jsAsset("").toString).stripSuffix(".js"))
+        }
+      )
+    org.webjars.RequireJS.getSetupJavaScript(routes.WebJarAssets.at("").url)
+      .replace("// All of the WebJar configs", s"requirejs.config($extra);")
+  }
+
+
 }
