51281ba4d428a75e707a6cb629df7e5f51da3cdc
Tim Dettrick <t.dettrick@uq.edu.au>
2016-12-12 14:47:11 +1000

    Handling access pass for unauthenticated session

--- dit4c-portal/app/controllers/AccessPassController.scala
+++ dit4c-portal/app/controllers/AccessPassController.scala
@@ -26,18 +26,29 @@ class AccessPassController(
   import play.api.libs.concurrent.Execution.Implicits._
 
   def redeemAccessPass(schedulerId: String, encodedAccessPass: String) =
-    silhouette.SecuredAction.async { implicit request =>
-      implicit val timeout = Timeout(1.minute)
-      for {
-        signedData <- Future { ByteString(Base64.getUrlDecoder.decode(encodedAccessPass)) }
-        queryMsg = UserSharder.Envelope(request.identity.id,
-            UserAggregate.AddAccessPass(schedulerId, signedData))
-        queryResponse <- (userSharder ? queryMsg)
-      } yield queryResponse match {
-        case UserAggregate.AccessPassAdded =>
-          Redirect(routes.MainController.index())
-        case UserAggregate.AccessPassRejected(reason) =>
-          InternalServerError(reason)
+    silhouette.UserAwareAction.async { implicit request =>
+      request.identity match {
+        case Some(identity) =>
+          implicit val timeout = Timeout(1.minute)
+          for {
+            signedData <- Future { ByteString(Base64.getUrlDecoder.decode(encodedAccessPass)) }
+            queryMsg = UserSharder.Envelope(identity.id,
+                UserAggregate.AddAccessPass(schedulerId, signedData))
+            queryResponse <- (userSharder ? queryMsg)
+          } yield queryResponse match {
+            case UserAggregate.AccessPassAdded =>
+              Redirect(routes.MainController.index())
+            case UserAggregate.AccessPassRejected(reason) =>
+              InternalServerError(reason)
+          }
+        case None =>
+          val redirectUri =
+            routes.AccessPassController.redeemAccessPass(
+                schedulerId, encodedAccessPass).absoluteURL
+          Future.successful {
+            Redirect(routes.MainController.index)
+              .withSession("redirect_uri" -> redirectUri)
+          }
       }
     }
 }
