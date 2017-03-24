7854c41bc6c6b0b92382b466ddad038123d8e5ac
Tim Dettrick <t.dettrick@uq.edu.au>
2017-03-24 14:05:41 +1000

    Update dependencies

--- project/SharedDependencyVersions.scala
+++ project/SharedDependencyVersions.scala
@@ -1,11 +1,11 @@
 object SharedDependencyVersions {
   val akkaV = "2.4.17"
-  val akkaHttpV = "10.0.3"
+  val akkaHttpV = "10.0.5"
   val chillV = "0.8.1"
   val ficusV = "1.4.0"
   val logbackV = "1.1.7"
   val macwireV = "2.2.3"
-  val playV = "2.5.12"
+  val playV = "2.5.13"
   val scalapbV = "0.5.43"
   val specs2V = "3.6.4"
 }
